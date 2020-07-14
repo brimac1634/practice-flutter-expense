abbfd1e7c38d30368451902d58df39c072965f5f
Brian MacPherson <brimac1634@gmail.com>
2020-07-14 19:56:38 +0800

    changed style of list items

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -11,9 +11,9 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(
-          
-          child: Text('\$${amount.toStringAsFixed(0)}')),
+        Container(
+          height: 20,
+            child: FittedBox(child: Text('\$${amount.toStringAsFixed(0)}'))),
         SizedBox(height: 4),
         Container(
           height: 60,
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -17,7 +17,9 @@ class TransactionList extends StatelessWidget {
               children: <Widget>[
                 Text('No Transactions Added Yet',
                     style: Theme.of(context).textTheme.title),
-                SizedBox(height: 10,),
+                SizedBox(
+                  height: 10,
+                ),
                 Container(
                     height: 200,
                     child: Image.asset(
@@ -29,40 +31,18 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                  elevation: 5.0,
-                  child: Row(children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Theme.of(context).primaryColorDark,
-                              width: 2)),
-                      child: Text(
-                        '\$${transactions[index].amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColorDark),
-                      ),
+                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                  elevation: 5,
+                                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(child: Text('\$${transactions[index].amount}'))),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transactions[index].title,
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(transactions[index].date),
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    )
-                  ]),
+                    title: Text(transactions[index].title, style: Theme.of(context).textTheme.title,),
+                    subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
+                  ),
                 );
               },
               itemCount: transactions.length,
