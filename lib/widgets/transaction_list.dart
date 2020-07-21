3b4a6fe92ebfab4ea5043c2b60b9e7a46b91a046
Brian MacPherson <brimac1634@gmail.com>
2020-07-21 10:05:45 +0800

    refactored transaction list items

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import './transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -33,37 +33,9 @@ class TransactionList extends StatelessWidget {
             })
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-                  elevation: 5,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                          padding: EdgeInsets.all(6),
-                          child: FittedBox(
-                              child: Text('\$${transactions[index].amount}'))),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date)),
-                    trailing: MediaQuery.of(context).size.width > 460
-                        ? FlatButton.icon(
-                            textColor: Theme.of(context).errorColor,
-                            onPressed: () =>
-                                deleteTransaction(transactions[index].id),
-                            icon: Icon(Icons.delete),
-                            label: Text('delete'))
-                        : IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () =>
-                                deleteTransaction(transactions[index].id),
-                            color: Theme.of(context).errorColor),
-                  ),
-                );
+                return TransactionItem(
+                    transaction: transactions[index],
+                    deleteTransaction: deleteTransaction);
               },
               itemCount: transactions.length,
             ),
